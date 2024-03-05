2f644eaebe38910d3787631b1a7d75f264665023
Andrea Andreeva <andreaandreeva13@gmail.com>
2024-03-05 18:34:47 +0200

    Created a frame for the game

---
+++ Pacman.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Pacman extends JFrame {
+static JFrame a;
+static JLabel l;
+
+
+public static void main(String[] args) {
+    a = new JFrame("Pac-man");
+    JPanel p = new JPanel();
+    ImageIcon originalIcon = new ImageIcon("Pac-Man-Logo.png");
+    Image originalImage = originalIcon.getImage();
+
+    int newWidth = 500;
+    int newHeight = 300;
+    Image resizedImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+
+    ImageIcon resizedIcon = new ImageIcon(resizedImage);
+
+
+    p.setBackground(Color.black);
+    l = new JLabel(resizedIcon);
+    a.add(p);
+    p.add(l);
+    a.setSize(800, 800);
+    a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    a.setVisible(true);
+    a.show();
+}
+}
+
