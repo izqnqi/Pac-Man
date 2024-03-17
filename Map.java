d4c71753681876ad0933757b358ed3ba512a5adf
Andrea Andreeva <andreaandreeva13@gmail.com>
2024-03-18 00:49:50 +0200

    .

--- Map.java
+++ Map.java
@@ -4,8 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Map extends JPanel implements ActionListener {
-    private int score;
-    private int lives;
+    private JLabel upL, downL, rightL, leftL, ghostBlueL, ghostRedL, ghostYellowL, ghostPinkL;
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -13,39 +13,32 @@ public class Map extends JPanel implements ActionListener {
     }
 
     private void showImages() {
-        ImageIcon up = new ImageIcon("images/pm-up.png");
-        ImageIcon down = new ImageIcon("images/pm-down.png");
-        ImageIcon right = new ImageIcon("images/pm-right.png");
-        ImageIcon left = new ImageIcon("images/pm-left.png");
-        ImageIcon ghostBlue = new ImageIcon("images/blue ghost.png");
-        ImageIcon ghostRed = new ImageIcon("images/red ghost.png");
-        ImageIcon ghostYellow = new ImageIcon("images/yellow ghost.png");
-        ImageIcon ghostPink = new ImageIcon("images/pink ghost.png");
+        upL = new JLabel(new ImageIcon("images/pm-up.png"));
+        downL = new JLabel(new ImageIcon("images/pm-down.png"));
+        rightL = new JLabel(new ImageIcon("images/pm-right.png"));
+        leftL = new JLabel(new ImageIcon("images/pm-left.png"));
+        ghostBlueL = new JLabel(new ImageIcon("images/blue ghost.png"));
+        ghostRedL = new JLabel(new ImageIcon("images/red ghost.png"));
+        ghostYellowL = new JLabel(new ImageIcon("images/yellow ghost.png"));
+        ghostPinkL = new JLabel(new ImageIcon("images/pink ghost.png"));
+
+        add(upL);
+        add(downL);
+        add(rightL);
+        add(leftL);
+        add(ghostBlueL);
+        add(ghostRedL);
+        add(ghostYellowL);
+        add(ghostPinkL);
+        setLayout(null);
+
+        upL.setBounds(30,30,20,60);
+        downL.setBounds(30,30,20,60);
+        ghostBlueL.setBounds(30,30,50,100);
     }
 
-    private void showScore(Graphics2D s) {
-        s.setColor(new Color(255, 255, 102));
-        s.setFont(new Font("Arial", Font.PLAIN, 15));
-        String string = "Score: " + score;
-        s.drawString(string, 20, 20);
-    }
-    private void showLives(Graphics2D l){
-        l.setColor(new Color(255, 255, 102));
-        l.setFont(new Font("Arial", Font.PLAIN, 15));
-        String string1 = "                                                                           Lives: " + lives;
-        l.drawString(string1,20,20);
-    }
 
-
-    public void mapTest() {
-        JPanel map = new JPanel();
-        map.setSize(200, 200);
-        map.setBorder(BorderFactory.createLineBorder(Color.blue));
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        showScore((Graphics2D) g);
-        showLives((Graphics2D) g);
+    public JLabel getGhostBlueL() {
+        return ghostBlueL;
     }
 }
--- Pacman.java
+++ Pacman.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 public class Pacman extends JFrame {
 static JFrame a;
 static JLabel l;
+static int lives;
+static int score;
 
 
 public static void main(String[] args) {
@@ -16,24 +18,37 @@ public static void main(String[] args) {
     int newHeight = 300;
     Image resizedImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
     ImageIcon resizedIcon = new ImageIcon(resizedImage);
-
     Map mapPanel = new Map();
-
 
 
     p.setBackground(Color.black);
     a.getContentPane().add(mapPanel);
-
     mapPanel.setBounds(0, 0, 400, 400);
     mapPanel.setBorder(BorderFactory.createLineBorder(Color.blue));
     mapPanel.setBackground(Color.black);
+
+    JLabel shlives = new JLabel(" Lives: " + lives, JLabel.LEFT);
+    shlives.setForeground(Color.yellow);
+    shlives.setFont(new Font("Arial", Font.PLAIN, 15));
+    mapPanel.add(shlives);
+    JLabel shscore = new JLabel("Score: " +score, JLabel.RIGHT);
+    shscore.setForeground(Color.yellow);
+    shscore.setFont(new Font("Arial", Font.PLAIN, 15));
+    mapPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+    mapPanel.add(shscore);
+
+  //  mapPanel.add(mapPanel.getGhostBlueL());
+
+
     l = new JLabel(resizedIcon);
+    p.add(l, BorderLayout.NORTH);
     a.add(p);
     p.add(l);
     a.setSize(800, 800);
     a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     a.setVisible(true);
     a.show();
+
 }
 }
 
